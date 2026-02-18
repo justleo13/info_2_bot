764a306b5d272fb6e7db57023014843e4d51d7ae
Obeleesq <blessql@icloud.com>
2026-02-18 08:31:01 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,64 @@
+import socket
+import os
+from pathlib import Path
+
+HOST = '0.0.0.0'
+PORT = 5001
+downloads_folder = str(Path.home() / "Downloads")
+
+server = socket.socket()
+server.bind((HOST, PORT))
+server.listen(5)
+
+print("Сервер запущен...")
+
+def recvall(conn, size):
+    data = b''
+    while len(data) < size:
+        packet = conn.recv(size - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+while True:
+    try:
+        conn, addr = server.accept()
+        print("Подключился:", addr)
+
+        name_size_bytes = recvall(conn, 4)
+        if not name_size_bytes:
+            print("Ошибка: не удалось получить размер имени")
+            conn.close()
+            continue
+
+        name_size = int.from_bytes(name_size_bytes, 'big')
+
+        filename_bytes = recvall(conn, name_size)
+        if not filename_bytes:
+            print("Ошибка: не удалось получить имя файла")
+            conn.close()
+            continue
+
+        filename = filename_bytes.decode('utf-8', errors='ignore')
+
+
+        if not filename:
+            print("Ошибка: пустое имя файла")
+            conn.close()
+            continue
+
+        filepath = os.path.join(downloads_folder, filename)
+
+        with open(filepath, "wb") as f:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                f.write(data)
+
+        print("Файл получен:", filename)
+        conn.close()
+
+    except Exception as e:
+        print("Ошибка сервера:", e)
